c4082f017c1f109b191df3b7ac7172411fba9c86
skzy2018 <skzy2018@gmail.com>
2025-04-26 22:05:39 +0900

    table data definition add unique keys

--- data/ddl/accounts.sql
+++ data/ddl/accounts.sql
@@ -3,6 +3,7 @@ CREATE TABLE accounts (
     name TEXT NOT NULL,               -- 口座名（銀行名・カード名）
     account_type TEXT NOT NULL,       -- 種類（銀行口座、クレジットカード等）
     currency TEXT DEFAULT 'JPY',
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(name)
 );
 
--- data/ddl/categories.sql
+++ data/ddl/categories.sql
@@ -2,5 +2,6 @@ CREATE TABLE categories (
     category_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,                -- 食費、住居費、給与など
     type TEXT NOT NULL,                -- 支出(expense), 収入(income)
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(type,name)
 );
